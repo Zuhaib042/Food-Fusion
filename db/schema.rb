b9c75db2385c3900ec5c1ad4b7422c9a55e43c33
Zuhaib Amjad <umar0423@gmail.com>
2023-02-06 18:11:03 +0500

    Add all the remaining migrations to complete schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_06_125115) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_06_130829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,11 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_06_125115) do
     t.string "name"
     t.string "measurement_unit"
     t.integer "price"
-    t.string "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "inventories", force: :cascade do |t|
@@ -38,6 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_06_125115) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "inventory_id", null: false
+    t.bigint "food_id", null: false
+    t.index ["food_id"], name: "index_inventory_foods_on_food_id"
     t.index ["inventory_id"], name: "index_inventory_foods_on_inventory_id"
   end
 
@@ -69,8 +68,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_06_125115) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "foods", "users"
   add_foreign_key "inventories", "users"
+  add_foreign_key "inventory_foods", "foods"
   add_foreign_key "inventory_foods", "inventories"
   add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
