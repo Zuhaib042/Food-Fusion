e1f1a88dc6808b666797a34a2600ea351a1fc993
Zuhaib Amjad <umar0423@gmail.com>
2023-02-07 15:32:37 +0500

    Install and setup devise for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password confirm_password])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[name password])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[name email password current_password])
+  end
 end
