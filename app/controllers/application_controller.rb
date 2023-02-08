5c633b73263f4fcdb7d3f79edfc17f59fed192f7
Zuhaib Amjad <umar0423@gmail.com>
2023-02-08 09:33:32 +0500

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-  # def after_sign_in_path_for(_resource)
-  #   user_url(current_user)
-  # end
-
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
   end
