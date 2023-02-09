4390a8ecc9b9d7149d58403f7f4a672b608cb4ff
Zuhaib Amjad <umar0423@gmail.com>
2023-02-09 18:19:39 +0500

    Update schema with migrations

--- db/migrate/20230206110751_add_user_ref_to_food.rb
+++ db/migrate/20230206110751_add_user_ref_to_food.rb
@@ -1,6 +1,6 @@
 class AddUserRefToFood < ActiveRecord::Migration[7.0]
   def change
-    add_reference :foods, :user, null: false, index: true, foreign_key: true
-    add_column :foods, :user_id, :integer
+    # add_reference :foods, :user, null: false, index: true, foreign_key: true
+    # add_column :foods, :user_id, :integer
   end
 end
