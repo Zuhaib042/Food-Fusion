19f2226c6feb5ccb736cf27181c46c5fcc3075b4
Zuhaib Amjad <umar0423@gmail.com>
2023-02-10 10:13:32 +0500

    Add recipe_foods variable in show method in recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,6 +6,7 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = current_user.recipes.find(params[:id])
+    @recipe_foods = @recipe.recipe_foods.includes(:recipe, :food)
   end
 
   def new
