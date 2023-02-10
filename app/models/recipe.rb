52e3e5c15bbc5eb2f928227a071a7d67e586460a
Zuhaib Amjad <umar0423@gmail.com>
2023-02-11 00:19:07 +0500

    Update inventory and recipe models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
+  has_many :foods, through: :recipe_foods
 
   validates :name, :cooking_time, :preparation_time, :description, presence: true
 end
