48a30e4de3f5acc9827aadb8a53884cf66c7af58
Faranosh Amini <faranosh.amini4@gmail.com>
2023-02-10 18:04:49 +0430

    Create inventory_foods_controller.rb

---
+++ app/controllers/inventory_foods_controller.rb
@@ -0,0 +1,33 @@
+class InventoryFoodsController < ApplicationController
+  def index
+    @InventoryFood = InventoryFood.all
+  end
+  
+  def create 
+    @foods = Food.all
+    @inventoryfood = InventoryFood.create(inventory_food_params)
+    @inventoryfood.inventory = Inventory.find(params[:Inventory_id])
+    if @inventoryfood.save
+      redirect_to inventory_url(@inventoryfood.inventory), notice: 'InventoryFood Successfully created'
+    else
+       render :new, status: :unprocessable_entity 
+    end 
+  end   
+  
+    def new
+      @inventoryfood = InventoryFood.new
+      @foods = Food.all
+    end   
+    
+    def destroy
+      food = InventoryFood.find(params[:id])
+      p food
+      food.destroy
+      redirect_to inventory_inventory_food_path
+    end 
+    
+    private
+    def inventory_food_params
+      params.require(:inventory_foods).permit(:quantity, food_id)
+    end    
+end    
