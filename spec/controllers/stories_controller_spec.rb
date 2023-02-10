39e3224971a08028c4afc585e5e5e4e1ff530c87
Zuhaib Amjad <umar0423@gmail.com>
2023-02-10 18:27:33 +0500

    Add factory bot into project add login testing of user with devise

---
+++ spec/controllers/stories_controller_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+require_relative "../support/devise"
+
+RSpec.describe RecipesController, type: :controller do
+  describe 'GET /' do
+    login_user
+
+    context "from login user" do
+      it "should return 200:OK" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+end
