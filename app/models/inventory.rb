f24dce04a6092c75016e0086f832dc8533e38df7
Zuhaib Amjad <umar0423@gmail.com>
2023-02-10 18:34:32 +0500

    Add validations to inventory and recipe models

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,6 @@
 class Inventory < ApplicationRecord
   belongs_to :user
   has_many :inventory_foods, dependent: :destroy
+
+  validates :name, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
+
+  validates :name, :cooking_time, :preparation_time, :description, presence: true
 end
