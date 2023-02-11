8daa5969cf232f798920966698dc35725219d2f2
Faranosh Amini <faranosh.amini4@gmail.com>
2023-02-11 14:58:40 +0430

    fix rubocop error

--- spec/request/foods_controller_spec.rb
+++ spec/request/foods_controller_spec.rb
@@ -1,27 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe Food, type: :request do 
-    before(:each) do
-        @user = User.create(name: 'Faranosh Amini', created_at: Time.now, updated_at: Time.now, email: 'faranosh.amini4@gmail.com', encrypted_password: '123456')
-        # @user.skip_confirmation!
-        @user.save
-        post @user_session_path, params:{user: {email: @user.email, password: @user.password}}
-        # sign_in @user
-        get foods_path
-    end   
-    describe 'Food Controller' do
-      it 'gets the index page' do 
-        expect(response).to have_http_status :ok
-      end
-      it 'renders the index page' do
-        expect(response).to render_template(:index)
-      end  
+RSpec.describe Food, type: :request do
+  before(:each) do
+    @user = User.create(name: 'Faranosh Amini', created_at: Time.now, updated_at: Time.now,
+                        email: 'faranosh.amini4@gmail.com', encrypted_password: '123456')
+    # @user.skip_confirmation!
+    @user.save
+    post @user_session_path, params: { user: { email: @user.email, password: @user.password } }
+    # sign_in @user
+    get foods_path
+  end
+  describe 'Food Controller' do
+    it 'gets the index page' do
+      expect(response).to have_http_status :ok
+    end
+    it 'renders the index page' do
+      expect(response).to render_template(:index)
+    end
     #   it 'Should have some html' do
     #     expect(response.body).to include '<table class="w-6/12 bg-stone-200">'
-    #   end  
-    # end 
+    #   end
+    # end
     # describe 'Testing the new method' do
-    #     before(:example) { get new_food_path } 
+    #     before(:example) { get new_food_path }
     #     it 'should redirect to add food page' do
     #         expect(response).to have_http_status :ok
     #     end
@@ -30,12 +31,12 @@ RSpec.describe Food, type: :request do
     #     end
     #     it 'Should have some html' do
     #         expect(response.body).to include '<th scope="col">Food</th>'
-    #     end        
+    #     end
     # end
     #  describe 'should create a food' do
     #     before(:each) do
-    #      @food = Food.new(name: 'Apple', measurement_unit: 'kg', price: '100', created_at: Time.now, updated_at: Time.now, user_id: @user.id)    
-    #     end  
+    #      @food = Food.new(name: 'Apple', measurement_unit: 'kg', price: '100', created_at: Time.now, updated_at: Time.now, user_id: @user.id)
+    #     end
     #     it 'Should create new food' do
     #         expect(@food).to be_valid
     #     end
@@ -51,7 +52,6 @@ RSpec.describe Food, type: :request do
     #         expect(@user.foods.count).to be 1
     #     @food.destroy
     #     expect(@user.foods.count).to be 0
-    #     end 
-    end
-end  
-  
+    #     end
+  end
+end
