1d8df8410df96bffca9528fcd422622c802eefb6
Faranosh Amini <faranosh.amini4@gmail.com>
2023-02-11 14:33:47 +0430

    Add test for foods controller

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,17 +1,11 @@
 class ShoppingListController < ApplicationController
-
   def index
     @recipe = Recipe.find(params[:recipe_id])
     @inventory = Inventory.find(params[:inventory_id])
     @missing_foods = []
-    
+
     @recipe.foods.each do |food|
-     unless @inventory.foods.include?(food)
-       @missing_foods.push(food)
-     end
+      @missing_foods.push(food) unless @inventory.foods.include?(food)
     end
-
   end
-  
-
 end
--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -1,12 +1,12 @@
-require "rails_helper"
-require_relative "../support/devise"
+require 'rails_helper'
+require_relative '../support/devise'
 
 RSpec.describe RecipesController, type: :controller do
   describe 'GET /' do
     login_user
 
-    context "from login user" do
-      it "should return 200:OK" do
+    context 'from login user' do
+      it 'should return 200:OK' do
         get :index
         expect(response).to have_http_status(:success)
       end
